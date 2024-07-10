393ef3bfbd79bae585e4525eb4ddb1e086e978a6
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2024-07-10 17:02:39 +0700

    feat: admin

---
+++ resources/views/admin/components/mobile-navbar.blade.php
@@ -0,0 +1,15 @@
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="mobilenavbar"
+        aria-labelledby="offcanvasWithBothOptionsLabel">
+        <nav class="sidebar-nav scroll-sidebar">
+            <div class="offcanvas-header justify-content-between">
+                <img src="../../dist/images/logos/favicon.ico" alt="" class="img-fluid">
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body profile-dropdown mobile-navbar" data-simplebar="" data-simplebar>
+                <div class="d-block d-lg-none">
+                    <img src="../../dist/images/logos/dark-logo.svg" class="dark-logo" width="180" alt="" />
+                    <img src="../../dist/images/logos/light-logo.svg" class="light-logo" width="180" alt="" />
+                  </div>
+            </div>
+        </nav>
+    </div>
---
+++ resources/views/admin/components/sidebar.blade.php
@@ -0,0 +1,28 @@
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+            <a href="./index.html" class="text-nowrap logo-img">
+                <text>Reimburshment App</text>
+                {{-- <img src="../../dist/images/logos/dark-logo.svg" class="dark-logo" width="180" alt="" />
+                <img src="../../dist/images/logos/light-logo.svg" class="light-logo" width="180" alt="" /> --}}
+            </a>
+            <div class="close-btn d-lg-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                <i class="ti ti-x fs-8 text-muted"></i>
+            </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar>
+            <ul id="sidebarnav">
+
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                    <span class="hide-menu">Home</span>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
