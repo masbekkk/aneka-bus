1089111bc73cf935488bf502ec33cbb406b505b8
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2024-07-11 09:18:18 +0700

    feat: footer

--- resources/views/landing-page/footer.blade.php
+++ resources/views/landing-page/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer-part pt-8 pb-5">
+{{-- <footer class="footer-part pt-8 pb-5">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-4">
@@ -10,6 +10,34 @@
                             class="text-dark text-hover-primary border-bottom border-primary"
                             href="https://anekatransport.com/">AnekaTransport.com</a></p>
                 </div>
+            </div>
+        </div>
+    </div>
+</footer> --}}
+<footer>
+    <!-- Container for the logo and operating hours -->
+    <div class="container d-flex flex-column align-items-center">
+        <div class="row w-100">
+            <!-- Logo -->
+            <div class="col-12 d-flex justify-content-center mb-3">
+                <img src="{{ asset('images/logo.png')}}"
+                    alt="Aneka Logo" width="672" height="189" class="img-fluid">
+            </div>
+        </div>
+        <div class="row w-100">
+            <!-- Operating Hours -->
+            <div class="col-12 d-flex flex-column align-items-center text-center">
+                <h3 class="mb-2">JAM OPERASIONAL</h3>
+                <p>Senin - Minggu<br>08:00 - 22:00 WITA</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer Bottom -->
+    <div class="container-fluid py-3 bg-header">
+        <div class="row">
+            <div class="col-12 text-center">
+                <span>Copyright © 2024 Aneka Transport | Powered by Aneka Transport</span>
             </div>
         </div>
     </div>
