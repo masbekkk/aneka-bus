066297d63b1ced66f28f2c1d1b20a23e64812de9
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2024-07-11 17:22:55 +0700

    feat: bg header

--- resources/views/admin/transaction/index.blade.php
+++ resources/views/admin/transaction/index.blade.php
@@ -59,11 +59,11 @@
             </div>
         </div>
     </div>
-    <div class="card bg-light-info shadow-none position-relative overflow-hidden">
+    <div class="card bg-header text-white shadow-none position-relative overflow-hidden">
         <div class="card-body px-4 py-3">
             <div class="row align-items-center">
                 <div class="col-9">
-                    <h4 class="fw-semibold mb-8">Data Transaksi</h4>
+                    <h4 class="fw-semibold mb-8 text-white">Data Transaksi</h4>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item" aria-current="page">Transaksi</li>
