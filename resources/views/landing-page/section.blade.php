20d2c52de2df4bffb6dd522d3432ca3da36573fd
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2024-07-11 12:49:04 +0700

    feat: remove aos

---
+++ resources/views/landing-page/section.blade.php
@@ -0,0 +1,256 @@
+<section class="review-section pt-5 pt-lg-10 pt-xl-12 pb-8 pb-lg-9">
+    <div class="container" id="tentang-kami">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <h2 class="fs-9 text-center mb-4 mb-lg-5 fw-bolder" >Kenapa Anda Harus Memiliki Kami
+                </h2>
+            </div>
+        </div>
+        <div class="review-slider" >
+            <div class="owl-carousel owl-theme">
+                <div class="item card-container">
+                    <div class="card">
+                        <div class="card-body p-4">
+                            <div class="d-flex justify-content-between mb-4">
+                                <div class="d-flex align-items-center">
+                                    <img src="{{ asset('dist/images/backgrounds/gold.png') }}" alt=""
+                                        class="w-auto me-3 rounded-circle" width="40" height="40">
+                                    <div>
+                                        <h6 class="fs-4 mb-1 fw-semibold">Keunggulan Kami</h6>
+                                        <p class="mb-0 text-dark">Kami Berikan yang terbaik</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <ul
+                                        class="list-unstyled d-flex align-items-center justify-content-end gap-1 mb-0">
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <p class="fs-4 mb-0 text-dark">Kru Bus yang berpengalaman dan ramah. Supir yang
+                                berpengalaman, serta pelayanan yang baik dan keramahan kru disetiap bus, akan
+                                menemani
+                                anda sepanjang perjalanan</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item card-container">
+                    <div class="card">
+                        <div class="card-body p-4">
+                            <div class="d-flex justify-content-between mb-4">
+                                <div class="d-flex align-items-center">
+                                    <img src="{{ asset('dist/images/backgrounds/rocket.png') }}" alt=""
+                                        class="w-auto me-3 rounded-circle" width="40" height="40">
+                                    <div>
+                                        <h6 class="fs-4 mb-1 fw-semibold">Armada Bus Terbaru Dan Terbaik</h6>
+                                        <p class="mb-0 text-dark">Kami berikan yang terbaik</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <ul
+                                        class="list-unstyled d-flex align-items-center justify-content-end gap-1 mb-0">
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <p class="fs-4 text-dark mb-0">Dukungan perusahaan ternama dan terbaik di dunia, Scania
+                                dan
+                                Mercedes Benz menjadikan Aneka Transport menjadi yang terbaik.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item card-container">
+                    <div class="card">
+                        <div class="card-body p-4">
+                            <div class="d-flex justify-content-between mb-4">
+                                <div class="d-flex align-items-center">
+                                    <img src="{{ asset('dist/images/breadcrumb/ChatBc.png') }}" alt=""
+                                        class="w-auto me-3 rounded-circle" width="40" height="40">
+                                    <div>
+                                        <h6 class="fs-4 mb-1 fw-semibold">Fasilitas Yang Lengkap Dan Bersih</h6>
+                                        <p class="mb-0 fw-normal">Kami berikan yang terbaik</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <ul
+                                        class="list-unstyled d-flex align-items-center justify-content-end gap-1 mb-0">
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <p class="fs-4 text-dark mb-0">Kabin yang bersih, fasilitas yang steril, serta keamanan
+                                menjadi perhatian kami untuk menemani perjalanan anda.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item card-container">
+                    <div class="card">
+                        <div class="card-body p-4">
+                            <div class="d-flex justify-content-between mb-4">
+                                <div class="d-flex align-items-center">
+                                    <img src="{{ asset('dist/images/backgrounds/piggy.png') }}" alt=""
+                                        class="w-auto me-3 rounded-circle" width="40" height="40">
+                                    <div>
+                                        <h6 class="fs-4 mb-1 fw-semibold">Harga Terjangkau</h6>
+                                        <p class="mb-0 text-dark">Kami berikan yang terbaik</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <ul
+                                        class="list-unstyled d-flex align-items-center justify-content-end gap-1 mb-0">
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                        <li><a href=""><img
+                                                    src="../landingpage/dist/images/svgs/icon-star.svg"
+                                                    alt="" class="img-fluid"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <p class="fs-4 mb-0 text-dark">Memberikan harga yang ramah di kantong dan tetap
+                                mengutamakan kenyamanan dan keamanan penumpang.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="features-section py-5">
+    <div class="container" id="armada-kami">
+        <div class="row justify-content-center mb-4">
+            <div class="col-lg-6">
+                <div class="text-center" >
+                    <small class="text-bg fw-bold mb-2 d-block fs-3 ">KANTOR PERWAKILAN KAMI</small>
+                    <h2 class="fs-9 text-center mb-4 mb-lg-9 fw-bolder">Kantor Perwakilan Aneka</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-4 col-lg-3" >
+                <div class="text-center mb-5">
+
+                    <h5 class="fs-5 fw-semibold mt-8 text-center">Kantor Perwakilan Makassar (Khusus Rute
+                        Makassar-Mamuju-Palopo)
+                    </h5>
+                    <p class="mb-0 text-dark text-center">Jalan Maccini Sawah, No 43 (Depan warung HR), Maccini
+                        gusung.
+                    </p>
+                    <p class="mb-0 text-dark">
+                        Telp : <a href="https://wa.me/6281244676434" class="text-bg">
+                            +6281244676434</a></p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="text-center mb-5">
+
+                    <h5 class="fs-5 fw-semibold mt-8">Kantor Perwakilan Makassar (Khusus Rute Makassar-Selayar)
+                    </h5>
+                    <p class="mb-0 text-dark text-center">Jalan Maccini Sawah, No 43 (Depan warung HR), Maccini
+                        gusung.
+                    </p>
+                    <p class="mb-0 text-dark">Telp : <a class="text-bg" href="https://wa.me/628114111170">
+                            +628114111170</a></p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3" >
+                <div class="text-center mb-5">
+
+                    <h5 class="fs-5 fw-semibold mt-8">Kantor Perwakilan Palopo</h5>
+                    <p class="mb-0 text-dark">Komplex Terminal Dangge, kelurahan dangge rakko, kecamatan wara
+                    </p>
+                    <p class="mb-0 text-dark">Telp : <a class="text-bg" href="https://wa.me/6281244676435">
+                            +6281244676435</a></p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3" >
+                <div class="text-center mb-5">
+
+                    <h5 class="fs-5 fw-semibold mt-8">Kantor perwakilan Mamuju</h5>
+                    <p class="mb-0 text-dark">Jalan Gatot Subroto, Komplex Terminal Tipe A Simbuang</p>
+                    <p class="mb-0 text-dark">Telp : <a class="text-bg" href="https://wa.me/6281244676435">
+                            +6281244676435</a></p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3" >
+                <div class="text-center mb-5">
+
+                    <h5 class="fs-5 fw-semibold mt-8">Kantor Perwakilan Selayar</h5>
+                    <p class="mb-0 text-dark">Jalan Kh. Hayyung No 66 (Depan Toko Senang Hati), Kec Benteng
+                    </p>
+                    <p class="mb-0 text-dark">Telp : <a class="text-bg" href="https://wa.me/62823456637777">
+                            +62823456637777</a></p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3" >
+                <div class="text-center mb-5">
+
+                    <h5 class="fs-5 fw-semibold mt-8">Kantor Perwakilan Makassar</h5>
+                    <p class="mb-0 text-dark">Jalan Maccini sawah no 43 (Depan Warung HR), Maccini Gusung</p>
+                    <p class="mb-0 text-dark">Telp : <a class="text-bg" href="https://wa.me/6285398537777">
+                            +6285398537777</a></p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3" >
+                <div class="text-center mb-5">
+
+                    <h5 class="fs-5 fw-semibold mt-8">Kantor Perwakilan Selayar</h5>
+                    <p class="mb-0 text-dark">Jalan Kh. Hayyung No 62 (Sebelah Cukur Madura), Kec Benteng</p>
+                    <p class="mb-0 text-dark">Telp : <a class="text-bg" href="https://wa.me/6285394245151">
+                            +6285394245151</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
