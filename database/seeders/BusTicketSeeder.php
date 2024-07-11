5136b7d5092dfef246ba912f29459fa5b4194c84
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2024-07-11 08:49:21 +0700

    feat: seeding tiket until 31 dec

--- database/seeders/BusTicketSeeder.php
+++ database/seeders/BusTicketSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\TicketBus;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,108 +14,116 @@ class BusTicketSeeder extends Seeder
      */
     public function run(): void
     {
-        TicketBus::create([
-            'route_source' => 1,
-            'route_destination' => 3,
-            'type_bus_id' => 1,
-            'departure_date' => '2024-07-11',
-            'departure_time' => '19:45:00',
-            'arrive_time' => '06:00:00',
-            'boarding_location' => 'Perwakilan Aneka Transport, Maccini Sawah',
-            'drop_location' => 'Kantor Perwakilan Aneka, Kompleks Terminal Tipe A Simbuang',
-            'price' => 260000,
-            // 'booked_seats' => implode(',', range(11, 14)),
-        ]);
+        // Set the start date as today
+        $startDate = Carbon::now();
+        // Set the end date as 31 December 2024
+        $endDate = Carbon::create(2024, 12, 31);
 
-        TicketBus::create([
-            'route_source' => 1,
-            'route_destination' => 3,
-            'type_bus_id' => 1,
-            'departure_date' => '2024-07-11',
-            'departure_time' => '19:45:00',
-            'arrive_time' => '06:00:00',
-            'boarding_location' => 'Perwakilan Terminal Daya',
-            'drop_location' => 'Kantor Perwakilan Aneka, Kompleks Terminal Tipe A Simbuang',
-            'price' => 260000,
-            // 'booked_seats' => implode(',', range(11, 15)),
-        ]);
+        // Iterate from the start date to the end date
+        for ($date = $startDate; $date->lte($endDate); $date->addDay()) {
+            TicketBus::create([
+                'route_source' => 1,
+                'route_destination' => 3,
+                'type_bus_id' => 1,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:45:00',
+                'arrive_time' => '06:00:00',
+                'boarding_location' => 'Perwakilan Aneka Transport, Maccini Sawah',
+                'drop_location' => 'Kantor Perwakilan Aneka, Kompleks Terminal Tipe A Simbuang',
+                'price' => 260000,
+                // 'booked_seats' => implode(',', range(11, 14)),
+            ]);
 
-        TicketBus::create([
-            'route_source' => 1,
-            'route_destination' => 3,
-            'type_bus_id' => 2,
-            'departure_date' => '2024-07-11',
-            'departure_time' => '19:45:00',
-            'arrive_time' => '06:00:00',
-            'boarding_location' => 'Perwakilan Aneka Transport, Maccini Sawah',
-            'drop_location' => 'Kantor Perwakilan Aneka, Kompleks Terminal Tipe A Simbuang',
-            'price' => 260000,
-            // 'booked_seats' => implode(',', range(11, 15)),
-        ]);
+            TicketBus::create([
+                'route_source' => 1,
+                'route_destination' => 3,
+                'type_bus_id' => 1,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:45:00',
+                'arrive_time' => '06:00:00',
+                'boarding_location' => 'Perwakilan Terminal Daya',
+                'drop_location' => 'Kantor Perwakilan Aneka, Kompleks Terminal Tipe A Simbuang',
+                'price' => 260000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
 
-        TicketBus::create([
-            'route_source' => 1,
-            'route_destination' => 3,
-            'type_bus_id' => 2,
-            'departure_date' => '2024-07-11',
-            'departure_time' => '19:45:00',
-            'arrive_time' => '06:00:00',
-            'boarding_location' => 'Perwakilan Terminal Daya',
-            'drop_location' => 'Kantor Perwakilan Aneka, Kompleks Terminal Tipe A Simbuang',
-            'price' => 260000,
-            // 'booked_seats' => implode(',', range(11, 15)),
-        ]);
+            TicketBus::create([
+                'route_source' => 1,
+                'route_destination' => 3,
+                'type_bus_id' => 2,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:45:00',
+                'arrive_time' => '06:00:00',
+                'boarding_location' => 'Perwakilan Aneka Transport, Maccini Sawah',
+                'drop_location' => 'Kantor Perwakilan Aneka, Kompleks Terminal Tipe A Simbuang',
+                'price' => 260000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
 
-        TicketBus::create([
-            'route_source' => 1,
-            'route_destination' => 2,
-            'type_bus_id' => 3,
-            'departure_date' => '2024-07-11',
-            'departure_time' => '19:30:00',
-            'arrive_time' => '06:00:00',
-            'boarding_location' => 'Perwakilan Aneka Transport, Maccini Sawah',
-            'drop_location' => 'Kantor Perwakilan Aneka, Ruko Kompleks Terminal Dangge',
-            'price' => 230000,
-            // 'booked_seats' => implode(',', range(11, 15)),
-        ]);
+            TicketBus::create([
+                'route_source' => 1,
+                'route_destination' => 3,
+                'type_bus_id' => 2,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:45:00',
+                'arrive_time' => '06:00:00',
+                'boarding_location' => 'Perwakilan Terminal Daya',
+                'drop_location' => 'Kantor Perwakilan Aneka, Kompleks Terminal Tipe A Simbuang',
+                'price' => 260000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
 
-        TicketBus::create([
-            'route_source' => 1,
-            'route_destination' => 2,
-            'type_bus_id' => 3,
-            'departure_date' => '2024-07-11',
-            'departure_time' => '19:30:00',
-            'arrive_time' => '06:00:00',
-            'boarding_location' => 'Perwakilan Terminal Daya',
-            'drop_location' => 'Kantor Perwakilan Aneka, Ruko Kompleks Terminal Dangge',
-            'price' => 230000,
-            // 'booked_seats' => implode(',', range(11, 15)),
-        ]);
+            TicketBus::create([
+                'route_source' => 1,
+                'route_destination' => 2,
+                'type_bus_id' => 3,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:30:00',
+                'arrive_time' => '06:00:00',
+                'boarding_location' => 'Perwakilan Aneka Transport, Maccini Sawah',
+                'drop_location' => 'Kantor Perwakilan Aneka, Ruko Kompleks Terminal Dangge',
+                'price' => 230000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
 
-        TicketBus::create([
-            'route_source' => 1,
-            'route_destination' => 2,
-            'type_bus_id' => 4,
-            'departure_date' => '2024-07-11',
-            'departure_time' => '19:30:00',
-            'arrive_time' => '06:00:00',
-            'boarding_location' => 'Perwakilan Aneka Transport, Maccini Sawah',
-            'drop_location' => 'Kantor Perwakilan Aneka, Ruko Kompleks Terminal Dangge',
-            'price' => 230000,
-            // 'booked_seats' => implode(',', range(11, 15)),
-        ]);
+            TicketBus::create([
+                'route_source' => 1,
+                'route_destination' => 2,
+                'type_bus_id' => 3,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:30:00',
+                'arrive_time' => '06:00:00',
+                'boarding_location' => 'Perwakilan Terminal Daya',
+                'drop_location' => 'Kantor Perwakilan Aneka, Ruko Kompleks Terminal Dangge',
+                'price' => 230000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
 
-        TicketBus::create([
-            'route_source' => 1,
-            'route_destination' => 2,
-            'type_bus_id' => 4,
-            'departure_date' => '2024-07-11',
-            'departure_time' => '19:30:00',
-            'arrive_time' => '06:00:00',
-            'boarding_location' => 'Perwakilan Terminal Daya',
-            'drop_location' => 'Kantor Perwakilan Aneka, Ruko Kompleks Terminal Dangge',
-            'price' => 230000,
-            // 'booked_seats' => implode(',', range(11, 15)),
-        ]);
+            TicketBus::create([
+                'route_source' => 1,
+                'route_destination' => 2,
+                'type_bus_id' => 4,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:30:00',
+                'arrive_time' => '06:00:00',
+                'boarding_location' => 'Perwakilan Aneka Transport, Maccini Sawah',
+                'drop_location' => 'Kantor Perwakilan Aneka, Ruko Kompleks Terminal Dangge',
+                'price' => 230000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
+
+            TicketBus::create([
+                'route_source' => 1,
+                'route_destination' => 2,
+                'type_bus_id' => 4,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:30:00',
+                'arrive_time' => '06:00:00',
+                'boarding_location' => 'Perwakilan Terminal Daya',
+                'drop_location' => 'Kantor Perwakilan Aneka, Ruko Kompleks Terminal Dangge',
+                'price' => 230000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
+        }
     }
 }
