8c313b5b66c183cfae231d739b49e4f3b9d053b4
denasgt123 <rohmantesting83@gmail.com>
2024-07-13 03:54:53 +0700

    fix: change section landingpage

--- resources/views/landing-page/section.blade.php
+++ resources/views/landing-page/section.blade.php
@@ -8,7 +8,7 @@
             </div>
         </div>
         <div class="review-slider" data-aos="fade-up" data-aos-delay="400" data-aos-duration="1000">
-            <div class="owl-carousel owl-theme">
+            {{-- <div class="owl-carousel owl-theme">
                 <div class="item card-container">
                     <div class="card">
                         <div class="card-body p-4">
@@ -164,23 +164,80 @@
                         </div>
                     </div>
                 </div>
+            </div> --}}
+            <div class="row row-cols-1 row-cols-lg-2 ">
+                <div class="col">
+                    <div class="card" style="min-height: 210px" style="min-height: 210px">
+                        <div class="card-body p-4">
+                            <div class="mb-2">
+                                <img src="{{ asset('dist/images/backgrounds/gold.png') }}" alt=""
+                                    class="w-auto rounded-circle mb-3" width="40" height="40">
+                                <h4 class="mb-1 fw-semibold">Keunggulan Kami</h4>
+                            </div>
+                            <p class="fs-4 mb-0 text-dark">Kru Bus yang berpengalaman dan ramah. Supir yang
+                                berpengalaman, serta pelayanan yang baik dan keramahan kru disetiap bus, akan
+                                menemani
+                                anda sepanjang perjalanan</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="min-height: 210px">
+                        <div class="card-body p-4">
+                            <div class="mb-2">
+                                <img src="{{ asset('dist/images/backgrounds/rocket.png') }}" alt=""
+                                    class="w-auto rounded-circle mb-3" width="40" height="40">
+                                <h4 class="mb-1 fw-semibold">Armada Bus Terbaru Dan Terbaik</h4>
+                            </div>
+                            <p class="fs-4 text-dark mb-0">Dukungan perusahaan ternama dan terbaik di dunia, Scania dan
+                                Mercedes Benz menjadikan Aneka Transport menjadi yang terbaik.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="min-height: 210px">
+                        <div class="card-body p-4">
+                            <div class="mb-2">
+                                <img src="{{ asset('dist/images/breadcrumb/ChatBc.png') }}" alt=""
+                                    class="w-auto rounded-circle mb-3" width="40" height="40">
+                                <h4 class="mb-1 fw-semibold">Fasilitas Yang Lengkap Dan Bersih</h4>
+                            </div>
+                            <p class="fs-4 mb-0 text-dark">Kabin yang bersih, fasilitas yang steril, serta keamanan
+                                menjadi perhatian kami untuk menemani perjalanan anda.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="min-height: 210px">
+                        <div class="card-body p-4">
+                            <div class="mb-2">
+                                <img src="{{ asset('dist/images/backgrounds/piggy.png') }}" alt=""
+                                    class="w-auto rounded-circle mb-3" width="40" height="40">
+                                <h4 class="mb-1 fw-semibold">Harga Terjangkau</h4>
+                            </div>
+                            <p class="fs-4 mb-0 text-dark">Memberikan harga yang ramah di kantong dan tetap
+                                mengutamakan kenyamanan dan keamanan penumpang.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </section>
 <section class="features-section py-5">
     <div class="container" id="armada-kami">
-        <div class="row justify-content-center mb-4">
-            <div class="col-lg-6">
+        <div class="row justify-content-center mb-3">
+            {{-- <div class="col-lg-6">
                 <div class="text-center" data-aos="fade-up" data-aos-delay="600" data-aos-duration="1000">
-                    <small class="text-bg fw-bold mb-2 d-block fs-3 ">KANTOR PERWAKILAN KAMI</small>
-                    <h2 class="fs-9 text-center mb-4 mb-lg-9 fw-bolder">Kantor Perwakilan Aneka</h2>
+                    <small class="text-bg fw-bold mb-2 d-block fs-3">KANTOR PERWAKILAN KAMI</small>
                 </div>
+            </div> --}}
+            <div data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
+                <small class="text-bg fw-bold d-block fs-3">KANTOR PERWAKILAN KAMI</small>
             </div>
         </div>
-        <div class="row">
-            <div class="col-sm-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="800"
-                data-aos-duration="1000">
+        {{-- <div class="row">
+            <div class="col-sm-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="800" data-aos-duration="1000">
                 <div class="text-center mb-5">
 
                     <h5 class="fs-5 fw-semibold mt-8 text-center">Kantor Perwakilan Makassar (Khusus Rute
@@ -194,8 +251,7 @@
                             +6281244676434</a></p>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="800"
-                data-aos-duration="1000">
+            <div class="col-sm-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="800" data-aos-duration="1000">
                 <div class="text-center mb-5">
 
                     <h5 class="fs-5 fw-semibold mt-8">Kantor Perwakilan Makassar (Khusus Rute Makassar-Selayar)
@@ -207,8 +263,7 @@
                             +628114111170</a></p>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="800"
-                data-aos-duration="1000">
+            <div class="col-sm-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="800" data-aos-duration="1000">
                 <div class="text-center mb-5">
 
                     <h5 class="fs-5 fw-semibold mt-8">Kantor Perwakilan Palopo</h5>
@@ -257,6 +312,141 @@
                     <p class="mb-0 text-dark">Jalan Kh. Hayyung No 62 (Sebelah Cukur Madura), Kec Benteng</p>
                     <p class="mb-0 text-dark">Telp : <a class="text-bg" href="https://wa.me/6285394245151">
                             +6285394245151</a></p>
+                </div>
+            </div>
+        </div> --}}
+        <div class="row row-cols-1 row-cols-lg-2">
+            <div class="col">
+                <h2 class="fs-8 mb-4 py-1 fw-bolder" data-aos="fade-up" data-aos-delay="400" data-aos-duration="1000">
+                    Kantor
+                    Perwakilan Aneka</h2>
+                <div class="mb-4" style="background-color: #ffb700; width: 216px; height: 4px;" data-aos="fade-up"
+                    data-aos-delay="400" data-aos-duration="1000"></div>
+                <div class="d-flex align-items-center justify-content-center mb-4" data-aos="fade-up"
+                    data-aos-delay="800" data-aos-duration="1000">
+                    <div class="me-4 rounded-circle" style="background-color: #ffb700; width: 19px; height: 19px;">
+                    </div>
+                    <div class="content flex-grow-1 pe-lg-3">
+                        <h5 class="mb-3" style="font-weight: 700;">Kantor Perwakilan Makassar (Khusus Rute
+                            Makassar-Mamuju-Palopo)
+                        </h5>
+                        <p class="fs-4 mb-1 text-dark">Jalan Maccini Sawah, No 43 (Depan warung HR), Maccini gusung.
+                        </p>
+                        <p class="fs-4 mb-1 text-dark">Telp : <a href="https://wa.me/6281244676434" class="text-bg"> +62
+                                81244676434</a></p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-4" data-aos="fade-up"
+                    data-aos-delay="800" data-aos-duration="1000">
+                    <div class="me-4 rounded-circle" style="background-color: #ffb700; width: 19px; height: 19px;">
+                    </div>
+                    <div class="content flex-grow-1 pe-lg-3">
+                        <h5 class="mb-3" style="font-weight: 700;">Kantor Perwakilan Makassar (Khusus Rute
+                            Makassar-Selayar)
+                        </h5>
+                        <p class="fs-4 mb-1 text-dark">Jalan Maccini Sawah, No 43 (Depan warung HR), Maccini gusung.
+                        </p>
+                        <p class="fs-4 mb-1 text-dark">Telp : <a class="text-bg" href="https://wa.me/628114111170">
+                                +62 8114111170</a></p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-4" data-aos="fade-up"
+                    data-aos-delay="800" data-aos-duration="1000">
+                    <div class="me-4 rounded-circle" style="background-color: #ffb700; width: 19px; height: 19px;">
+                    </div>
+                    <div class="content flex-grow-1 pe-lg-3">
+                        <h5 class="mb-3" style="font-weight: 700;">Kantor Perwakilan Palopo
+                        </h5>
+                        <p class="fs-4 mb-1 text-dark">Komplex Terminal Dangge, kelurahan dangge rakko, kecamatan wara
+                        </p>
+                        <p class="fs-4 mb-1 text-dark">Telp : <a class="text-bg" href="https://wa.me/6281244676435">
+                                +62 81244676435</a></p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-4" data-aos="fade-up"
+                    data-aos-delay="800" data-aos-duration="1000">
+                    <div class="me-4 rounded-circle" style="background-color: #ffb700; width: 19px; height: 19px;">
+                    </div>
+                    <div class="content flex-grow-1 pe-lg-3">
+                        <h5 class="mb-3" style="font-weight: 700;">Kantor perwakilan Mamuju
+                        </h5>
+                        <p class="fs-4 mb-1 text-dark">Jalan Gatot Subroto, Komplex Terminal Tipe A Simbuang
+                        </p>
+                        <p class="fs-4 mb-1 text-dark">Telp : <a class="text-bg" href="https://wa.me/6281244676435">
+                                +62 81244676435</a></p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-4" data-aos="fade-up"
+                    data-aos-delay="800" data-aos-duration="1000">
+                    <div class="me-4 rounded-circle" style="background-color: #ffb700; width: 19px; height: 19px;">
+                    </div>
+                    <div class="content flex-grow-1 pe-lg-3">
+                        <h5 class="mb-3" style="font-weight: 700;">Kantor Perwakilan Selayar
+                        </h5>
+                        <p class="fs-4 mb-1 text-dark">Jalan Kh. Hayyung No 66 (Depan Toko Senang Hati), Kec Benteng
+                        </p>
+                        <p class="fs-4 mb-1 text-dark">Telp : <a class="text-bg" href="https://wa.me/62823456637777">
+                                +62 823456637777</a></p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-4" data-aos="fade-up"
+                    data-aos-delay="800" data-aos-duration="1000">
+                    <div class="me-4 rounded-circle" style="background-color: #ffb700; width: 19px; height: 19px;">
+                    </div>
+                    <div class="content flex-grow-1 pe-lg-3">
+                        <h5 class="mb-3" style="font-weight: 700;">Kantor Perwakilan Makassar
+                        </h5>
+                        <p class="fs-4 mb-1 text-dark">Jalan Maccini sawah no 43 (Depan Warung HR), Maccini Gusung
+                        </p>
+                        <p class="fs-4 mb-1 text-dark">Telp : <a class="text-bg" href="https://wa.me/6285398537777">
+                                +62 85398537777</a></p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-4" data-aos="fade-up"
+                    data-aos-delay="800" data-aos-duration="1000">
+                    <div class="me-4 rounded-circle" style="background-color: #ffb700; width: 19px; height: 19px;">
+                    </div>
+                    <div class="content flex-grow-1 pe-lg-3">
+                        <h5 class="mb-3" style="font-weight: 700;">Kantor Perwakilan Selayar
+                        </h5>
+                        <p class="fs-4 mb-1 text-dark">Jalan Kh. Hayyung No 62 (Sebelah Cukur Madura), Kec Benteng
+                        </p>
+                        <p class="fs-4 mb-1 text-dark">Telp : <a class="text-bg" href="https://wa.me/6285394245151">
+                                +62 85394245151</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <h2 class="fs-8 mb-4 py-1 fw-bolder" data-aos="fade-up" data-aos-delay="400"
+                    data-aos-duration="1000">
+                    Kantor Perwakilan Sejahtera</h2>
+                <div class="mb-4" style="background-color: #ffb700; width: 216px; height: 4px;" data-aos="fade-up"
+                    data-aos-delay="400" data-aos-duration="1000"></div>
+                <div class="d-flex align-items-center justify-content-center mb-4" data-aos="fade-up"
+                    data-aos-delay="800" data-aos-duration="1000">
+                    <div class="me-4 rounded-circle" style="background-color: #ffb700; width: 19px; height: 19px;">
+                    </div>
+                    <div class="content flex-grow-1 pe-lg-3">
+                        <h5 class="mb-3" style="font-weight: 700;">Kantor Perwakilan Makassar
+                        </h5>
+                        <p class="fs-4 mb-1 text-dark">Jalan Maccini sawah no 43 (Depan Warung HR), Maccini Gusung
+                        </p>
+                        <p class="fs-4 mb-1 text-dark">Telp : <a href="https://wa.me/6285398537777" class="text-bg">
+                                +62 85398537777</a></p>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-4" data-aos="fade-up"
+                    data-aos-delay="800" data-aos-duration="1000">
+                    <div class="me-4 rounded-circle" style="background-color: #ffb700; width: 19px; height: 19px;">
+                    </div>
+                    <div class="content flex-grow-1 pe-lg-3">
+                        <h5 class="mb-3" style="font-weight: 700;">Kantor Perwakilan Selayar
+                        </h5>
+                        <p class="fs-4 mb-1 text-dark">Jalan Kh. Hayyung No 62 (Sebelah Cukur Madura), Kec Benteng
+                        </p>
+                        <p class="fs-4 mb-1 text-dark">Telp : <a href="https://wa.me/6285394245151" class="text-bg">
+                                +62 85394245151</a></p>
+                    </div>
                 </div>
             </div>
         </div>
