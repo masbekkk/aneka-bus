920de8d89288e7c673af4668ce1388f048e19b58
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2024-07-13 14:37:31 +0700

    feat: new busticket

--- database/seeders/BusTicketSeeder.php
+++ database/seeders/BusTicketSeeder.php
@@ -34,19 +34,6 @@ class BusTicketSeeder extends Seeder
                 // 'booked_seats' => implode(',', range(11, 14)),
             ]);
 
-            // TicketBus::create([
-            //     'route_source' => 1,
-            //     'route_destination' => 3,
-            //     'type_bus_id' => 1,
-            //     'departure_date' => $date->toDateString(),
-            //     'departure_time' => '19:45:00',
-            //     'arrive_time' => '06:00:00',
-            //     'boarding_location' => '',
-            //     'drop_location' => 'Kantor Perwakilan Aneka, Kompleks Terminal Tipe A Simbuang',
-            //     'price' => 260000,
-            //     // 'booked_seats' => implode(',', range(11, 15)),
-            // ]);
-
             TicketBus::create([
                 'route_source' => 1,
                 'route_destination' => 3,
@@ -60,18 +47,31 @@ class BusTicketSeeder extends Seeder
                 // 'booked_seats' => implode(',', range(11, 15)),
             ]);
 
-            // TicketBus::create([
-            //     'route_source' => 1,
-            //     'route_destination' => 3,
-            //     'type_bus_id' => 2,
-            //     'departure_date' => $date->toDateString(),
-            //     'departure_time' => '19:45:00',
-            //     'arrive_time' => '06:00:00',
-            //     'boarding_location' => '',
-            //     'drop_location' => 'Kantor Perwakilan Aneka, Kompleks Terminal Tipe A Simbuang',
-            //     'price' => 260000,
-            //     // 'booked_seats' => implode(',', range(11, 15)),
-            // ]);
+            TicketBus::create([
+                'route_source' => 3,
+                'route_destination' => 1,
+                'type_bus_id' => 1,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:45:00',
+                'arrive_time' => '06:00:00',
+                'drop_location' => 'Perwakilan Aneka Transport (Maccini Sawah), Perwakilan Terminal Daya',
+                'boarding_location' => 'Kantor Perwakilan Aneka (Kompleks Terminal Tipe A Simbuang)',
+                'price' => 260000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
+
+            TicketBus::create([
+                'route_source' => 3,
+                'route_destination' => 1,
+                'type_bus_id' => 2,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:45:00',
+                'arrive_time' => '06:00:00',
+                'drop_location' => 'Perwakilan Aneka Transport (Maccini Sawah), Perwakilan Terminal Daya',
+                'boarding_location' => 'Kantor Perwakilan Aneka (Kompleks Terminal Tipe A Simbuang)',
+                'price' => 260000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
 
             TicketBus::create([
                 'route_source' => 1,
@@ -86,19 +86,6 @@ class BusTicketSeeder extends Seeder
                 // 'booked_seats' => implode(',', range(11, 15)),
             ]);
 
-            // TicketBus::create([
-            //     'route_source' => 1,
-            //     'route_destination' => 2,
-            //     'type_bus_id' => 3,
-            //     'departure_date' => $date->toDateString(),
-            //     'departure_time' => '19:30:00',
-            //     'arrive_time' => '06:00:00',
-            //     'boarding_location' => 'Perwakilan Terminal Daya',
-            //     'drop_location' => 'Kantor Perwakilan Aneka, Ruko Kompleks Terminal Dangge',
-            //     'price' => 230000,
-            //     // 'booked_seats' => implode(',', range(11, 15)),
-            // ]);
-
             TicketBus::create([
                 'route_source' => 1,
                 'route_destination' => 2,
@@ -112,18 +99,32 @@ class BusTicketSeeder extends Seeder
                 // 'booked_seats' => implode(',', range(11, 15)),
             ]);
 
-            // TicketBus::create([
-            //     'route_source' => 1,
-            //     'route_destination' => 2,
-            //     'type_bus_id' => 4,
-            //     'departure_date' => $date->toDateString(),
-            //     'departure_time' => '19:30:00',
-            //     'arrive_time' => '06:00:00',
-            //     'boarding_location' => 'Perwakilan Terminal Daya',
-            //     'drop_location' => 'Kantor Perwakilan Aneka, Ruko Kompleks Terminal Dangge',
-            //     'price' => 230000,
-            //     // 'booked_seats' => implode(',', range(11, 15)),
-            // ]);
+
+            TicketBus::create([
+                'route_source' => 2,
+                'route_destination' => 1,
+                'type_bus_id' => 3,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:30:00',
+                'arrive_time' => '05:00:00',
+                'boarding_location' => 'Kantor Perwakilan Aneka (Ruko Kompleks Terminal Dangge)',
+                'drop_location' => 'Perwakilan Aneka Transport (Maccini Sawah), Perwakilan Terminal Daya',
+                'price' => 230000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
+
+            TicketBus::create([
+                'route_source' => 2,
+                'route_destination' => 1,
+                'type_bus_id' => 4,
+                'departure_date' => $date->toDateString(),
+                'departure_time' => '19:30:00',
+                'arrive_time' => '05:00:00',
+                'boarding_location' => 'Kantor Perwakilan Aneka (Ruko Kompleks Terminal Dangge)',
+                'drop_location' => 'Perwakilan Aneka Transport (Maccini Sawah), Perwakilan Terminal Daya',
+                'price' => 230000,
+                // 'booked_seats' => implode(',', range(11, 15)),
+            ]);
         }
     }
 }
