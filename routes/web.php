b9871103db9916a7c13bc17e936c459234aef916
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2024-07-24 16:27:52 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -12,27 +12,11 @@ use Illuminate\Support\Facades\Route;
 // });
 
 require __DIR__ . '/auth.php';
-require __DIR__ . '/coba.php';
-
-Route::get('/', function () {
-    $routes = BusRoute::orderBy('route_name', 'ASC')->get();
-    return view('landing-page.index', compact('routes'));
-})->name('landing-page');
-
-Route::get('/armada-kami', function () {
-    return view('landing-page.armada-kami');
-})->name('armada-kami');
-
-Route::get('/pusat-bantuan', function () {
-    return view('landing-page.pusat-bantuan');
-})->name('pusat-bantuan');
-
-Route::get('/admin', function () {
-    $routes = BusRoute::all();
-    return view('admin.ticket.index', compact('routes'));
-});
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', function() {
+        return redirect()->route('admin-order.index');
+    });
     Route::get('/admin-order/detail-passenger/{id}', [AdminController::class, 'passenger'])->name('admin.detail-passenger');
     Route::resource('admin-order', AdminController::class)->except(['show']);
     Route::get('/admin-order/{id}', [TicketBusController::class, 'chooseSeat'])->name('admin-order.show');
@@ -42,8 +26,5 @@ Route::middleware('auth')->group(function () {
     Route::get('/cetak-tiket/{id}', [BusReservationController::class, 'cetak_tiket'])->name('admin-order.cetak-tiket');
 });
 
-Route::resource('tiket-bus', TicketBusController::class);
+// Route::resource('tiket-bus', TicketBusController::class);
 Route::get('choose-seat/tiket-bus/{id}', [TicketBusController::class, 'chooseSeat'])->name('choose-seat.ticket-bus');
-
-
-Route::get('/detail-passenger/{id}', [BusReservationController::class, 'show'])->name('detail-passenger.ticket-bus');
